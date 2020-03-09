3a2a0bf3b15b92e0e3d500c6bea927b4ee57d446
bala <bala@10.1.7.16>
2020-03-09 13:23:52 +0530

    new line added

--- BankingApplication/src/Banking_Application/BankingApp.java
+++ BankingApplication/src/Banking_Application/BankingApp.java
@@ -74,6 +74,7 @@ class BankAccount
 			char option = '\0';
 			
 			System.out.println();
+			System.out.println();
 			System.out.println("Welcome : "+ customername);
 			System.out.println("Your Id : "+ customerid);
 			System.out.println();
